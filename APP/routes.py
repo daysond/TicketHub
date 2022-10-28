03c87c3528640db809b09f41ae99ae4a837b2534
daysond <daysond@outlook.com>
2022-10-27 23:18:51 -0400

    added footer, edited home

--- APP/routes.py
+++ APP/routes.py
@@ -1,7 +1,7 @@
 from App import app
 from flask import render_template, redirect, url_for, flash, get_flashed_messages, request
 from flask_login import login_user, logout_user, login_required, current_user
-from App.models import Concert, User
+from App.models import Concert, User, Venue, Event
 # from App.forms import RegisterForm, LoginForm, PurchaseForm
 from App import db
 
@@ -9,10 +9,12 @@ from App import db
 @app.route('/')
 @app.route('/home')
 def home():
-    print('called home')
-    return render_template('home.html')
+    feat_venues = Venue.query.all() #filter by feature
+    feat_events = Event.query.all()
+    return render_template('home.html', venues = feat_venues, events = feat_events)
 
 @app.route('/venues')
 def venue():
     print('called venue')
     return 'Venues'
+
