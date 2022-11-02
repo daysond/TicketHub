078b207300df6555f07f805a82b69af6ef71eb47
Kannav Sethi <kannavsethi2@gmail.com>
2022-11-02 13:02:18 -0400

    Made changes

--- APP/routes.py
+++ APP/routes.py
@@ -1,9 +1,9 @@
-from App import app
+from APP import app
 from flask import render_template, redirect, url_for, flash, get_flashed_messages, request
 from flask_login import login_user, logout_user, login_required, current_user
-from App.models import Concert, User, Venue, Event, Ticket, Seating
-from App.forms import PurchaseForm
-from App import db
+from APP.models import Concert, User, Venue, Event, Ticket, Seating
+from APP.forms import PurchaseForm
+from APP import db
 
 
 @app.route('/')
